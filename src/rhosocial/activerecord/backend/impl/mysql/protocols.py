ddba4994f1dc27c73dd484f887ffe16fcbae90a1
zhao vistart <vistart@rho.social>
2026-09-11 11:07:03 +0800

    refactor: remove extra dialect_options param from format_create_table_statement protocol

--- src/rhosocial/activerecord/backend/impl/mysql/protocols.py
+++ src/rhosocial/activerecord/backend/impl/mysql/protocols.py
@@ -317,9 +317,7 @@ class MySQLTableSupport(TableSupport, Protocol):
         """
         ...
 
-    def format_create_table_statement(
-        self, expr, dialect_options: Optional[Dict[str, Any]] = None
-    ) -> Tuple[str, tuple]:
+    def format_create_table_statement(self, expr) -> Tuple[str, tuple]:
         """Format CREATE TABLE statement."""
         ...
 
