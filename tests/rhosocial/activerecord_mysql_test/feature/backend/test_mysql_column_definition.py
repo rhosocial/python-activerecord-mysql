37c32daa6149a74d75eb328c2eedd9ab72082b20
zhao vistart <vistart@rho.social>
2026-09-23 21:50:04 +0800

    refactor(ddl): use core typed comment clause nodes
    Inline table/column comments are now TableCommentClause / ColumnCommentClause
    nodes; render them through format_table_comment_clause /
    format_column_comment_clause. Tests updated to build the clause nodes.

--- tests/rhosocial/activerecord_mysql_test/feature/backend/test_mysql_column_definition.py
+++ tests/rhosocial/activerecord_mysql_test/feature/backend/test_mysql_column_definition.py
@@ -4,6 +4,7 @@
 import pytest
 
 from rhosocial.activerecord.backend.expression import ColumnDefinition
+from rhosocial.activerecord.backend.expression import ColumnCommentClause
 from rhosocial.activerecord.backend.expression.types import VarCharType
 from rhosocial.activerecord.backend.impl.mysql.expression import (
     MySQLColumnDefinition,
@@ -66,7 +67,7 @@ def test_combined_attributes(dialect):
 
 def test_generic_column_still_renders_on_mysql(dialect):
     """MySQL format_column_definition accepts the generic ColumnDefinition too."""
-    generic = ColumnDefinition(dialect, "name", VarCharType(dialect, length=50), comment="c")
+    generic = ColumnDefinition(dialect, "name", VarCharType(dialect, length=50), comment=ColumnCommentClause(dialect, "c"))
     sql, _ = generic.to_sql()
     assert sql == "`name` VARCHAR(50) COMMENT 'c'"
 
